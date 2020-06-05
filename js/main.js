38e6d62cbd35ef051a895f23fc69877d831191ff
DanWttvn <dan.wttvn@gmail.com>
2020-06-05 16:22:46 +0200

    feat: cursor effect | selector effect

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
-/* <---------- Timeline 2 ----------> */
+
+
+/* <---------- Cursor ----------> */
+(function() {
+	const cursor = document.querySelector("#cursor");
+	hoverTargets = document.querySelectorAll(".hover-target");
+	
+
+	const setCursor = e => {
+		const { clientX: x, clientY: y } = e;
+		cursor.style.left = `${x}px`
+		cursor.style.top = `${y}px`
+	}
+	
+	const addHover = () => cursor.classList.add("hover")
+	const removeHover = () => cursor.classList.remove("hover")
+	
+	window.addEventListener("mousemove", setCursor)
+	hoverTargets.forEach(target => {
+		target.addEventListener("mouseover", addHover)
+		target.addEventListener("mouseout", removeHover)
+	});
+	
+})();
+
+
+
+/* <---------- Typed ----------> */
+
+var typed = new Typed('#typed', {
+	stringsElement: '#typed-strings',
+	typeSpeed: 60,
+	backSpeed: 60,
+	backDelay: 2e3,
+	loop: true,
+	shuffle: false,
+	showCursor: false
+});
