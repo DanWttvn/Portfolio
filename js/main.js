d48520ebf026d0a4e47dbb3c6e524fbd7b2b9142
DanWttvn <dan.wttvn@gmail.com>
2020-06-23 00:50:47 +0200

    feat: showcase functionality

--- js/main.js
+++ js/main.js
@@ -24,6 +24,18 @@
 })();
 
 
+/* <---------- Scroll Reveal ----------> */
+
+window.sr = ScrollReveal();
+sr.reveal(".showcase-bottom", {
+	duration: 2000,
+	origin: "bottom",
+	distance: "10px"
+});
+sr.reveal(".info-right", {
+	// delay: 2000, // desde que llego con el scroll
+	// viewFactor: 0.2 // cuando vemos el 2% del elemento, hace el efecto
+});
 
 /* <---------- Typed ----------> */
 
@@ -36,3 +48,39 @@ var typed = new Typed('#typed', {
 	shuffle: false,
 	showCursor: false
 });
+
+
+
+/* <---------- Projects Showcase ----------> */
+let sliderIndex = 0;
+const projectsWrapper = document.querySelector(".projects-wrapper")
+const numProjects = document.querySelectorAll(".project").length
+
+const controlPrev = document.querySelector(".control-prev");
+const controlNext = document.querySelector(".control-next");
+
+controlPrev.addEventListener("click", () => switchEvent("prev"))
+controlNext.addEventListener("click", () => switchEvent("next"))
+
+const currentWrapper = document.querySelector(".timeline .indicator .current-wrapper")
+const totalProjects = document.querySelector(".timeline .indicator .out-of")
+
+// Create indicator
+totalProjects.innerText = `0${numProjects}`
+for (let i = 0; i < numProjects; i++) {
+	const text = document.createElement("div")
+	text.innerText = `0${i+1}`
+	currentWrapper.appendChild(text)
+}
+
+// Switch Event
+function switchEvent(dir) {
+	
+	if(dir === "next" && sliderIndex === numProjects - 1) { sliderIndex = 0 } 
+	else if (dir === "prev" && sliderIndex === 0) { sliderIndex = 0 } 
+	else if (dir === "next") { sliderIndex++ } 
+	else { sliderIndex-- }
+
+	projectsWrapper.style.transform = "translateX(" + -sliderIndex + "00%)";
+	currentWrapper.style.transform = "translateY(" + -sliderIndex/numProjects * 100 + "%)";
+}
